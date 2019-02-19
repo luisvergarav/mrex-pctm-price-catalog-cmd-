11cbfc6fddba6f3e94b3595d42dd09144c3c0787
ext_lavergarav <ext_lavergarav@MOEXT_LAVER.Falabella.com>
2019-02-19 15:24:46 -0300

    Initial commit

---
+++ mrex-pctm-price-catalog-cmd/src/main/java/rtl/tot/corp/mrex/pctm/price/catalog/mrexpctmpricecatalogcmd/MrexPctmPriceCatalogCmdApplication.java
@@ -0,0 +1,13 @@
+package rtl.tot.corp.mrex.pctm.price.catalog.mrexpctmpricecatalogcmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrexPctmPriceCatalogCmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrexPctmPriceCatalogCmdApplication.class, args);
+	}
+
+}
---
+++ mrex-pctm-price-catalog-cmd/src/test/java/rtl/tot/corp/mrex/pctm/price/catalog/mrexpctmpricecatalogcmd/MrexPctmPriceCatalogCmdApplicationTests.java
@@ -0,0 +1,16 @@
+package rtl.tot.corp.mrex.pctm.price.catalog.mrexpctmpricecatalogcmd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MrexPctmPriceCatalogCmdApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
